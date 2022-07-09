e7f78e07222446e2a63432164ce0b11e289b04c7
Hafael <rafaell14062002@gmail.com>
2022-07-09 14:24:31 -0300

    Gold on HUD

---
+++ PrototypeLSW/Assets/Scripts/Managers/HandlerUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Managers
+{
+    public class HandlerUI : MonoBehaviour
+    {
+        [SerializeField] private InventorySO playerInventory;
+        [SerializeField] private TextMeshProUGUI goldText;
+
+        private void Update()
+        {
+            goldText.text = $"{playerInventory.gold}x";
+        }
+    }
+}
